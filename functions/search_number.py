4fd08a168d43a64e39ce1a540daba20040e4bbcf
jawad219 <jawadbinmujib219@gmail.com>
2025-09-21 11:53:18 +0600

    updated few lines

--- functions/search_number.py
+++ functions/search_number.py
@@ -1,8 +1,12 @@
+
 def is_james_bond(l1):
     for num in range(len(l1)-2):
         if l1[num] == 0 and l1[num + 1] == 0 and l1[num+2] == 7:
             return True
-
+        elif l1[num] == 0 and l1[num+2] == 0 and l1[num+4] == 7:
+            return True
+        elif l1[num+1] == 0 and l1[num+3] == 0 and l1[num+5] == 7:
+            return True
     return False
 
 
