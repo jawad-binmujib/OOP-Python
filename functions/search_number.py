bb8e986390b12c3d9c46f739f042b5e1057d069b
jawad219 <jawadbinmujib219@gmail.com>
2025-09-21 11:52:08 +0600

    search 007 in given list

---
+++ functions/search_number.py
@@ -0,0 +1,10 @@
+def is_james_bond(l1):
+    for num in range(len(l1)-2):
+        if l1[num] == 0 and l1[num + 1] == 0 and l1[num+2] == 7:
+            return True
+
+    return False
+
+
+
+is_james_bond([1, 0, 2, 0, 4, 7, 5] )
