7077e643abc7e3392d4bce171dac1b073749618a
jawad219 <jawadbinmujib219@gmail.com>
2025-10-10 01:10:55 +0600

    refactor Calculator class for improved readability and functionality

--- instance_method & overloading/calculator.py
+++ instance_method & overloading/calculator.py
@@ -1,43 +1,44 @@
-class calculator:
-    def product(self, num1, num2 = None, num3 = None):
-        if num1 != None and num2 != None and num3 != None:
-            print(num1*num2*num3)
-        elif num1 != None and num2 != None:
-            print(num1 * num2)
+# The asterisk (*) allows the method to accept any number of arguments.
+# This is useful when you don't know beforehand how many inputs the user will provide.
+
+class Calculator:
+    def product(self, num1, num2=None, num3=None):
+        if num1 is not None and num2 is not None and num3 is not None:
+            print(f"product is:{num1 * num2 * num3}")
+        elif num1 is not None and num2 is not None:
+            print(f"product is:{num1 * num2}")
         else:
-            print(1*num1)
+            print(f"product is:{1 * num1}")
 
-int1 = calculator()
-int1.product(6)
-int1.product(6,8)
-
-####but what if we take n number of arguments?? for that we need to use asterisk(*) so that we can take infinite number of arguments
-
-class calculator:
-
-    def __init__(self):
-        pass
     def prod(self, *nums):
         pro = 1
         for i in nums:
-            pro*= i
+            pro *= i
         print(f"product is:{pro}")
+
     def addition(self, *numb):
         sum = 0
         for j in numb:
-            sum+=j
+            sum += j
         print(f"summation is:{sum}")
 
     def subtraction(self, *num):
-        subs= 0
-        for x in num:
-            subs = x - subs
-        print(subs)
+        if not num:
+            print("No numbers provided")
+            return
+        subs = num[0]
+        for x in num[1:]:
+            subs -= x
+        print(f"subtraction is:{subs}")
 
+# Usage
+int1 = Calculator()
+int1.product(6)
+int1.product(6, 8)
 
+print('----------------------------')
 
-int2 = calculator()
-
-int2.prod(5,6,7)
-int2.addition(5,6,7)
-int2.subtraction(5,6,7)
+int2 = Calculator()
+int2.prod(5, 6, 7)
+int2.addition(5, 6, 7)
+int2.subtraction(5, 6, 7)
